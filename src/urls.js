e8ea70c089b22893bf7c1648a9ae5d3a437086f9
Ankit Kumar <ankitkumar1578114@gmail.com>
2022-03-07 19:58:09 +0530

    urllll

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,3 @@
-//const BackendUrl = "https://quick-finder-backend.herokuapp.com";
-const BackendUrl = "http://localhost:4000";
+const BackendUrl = "https://quick-finder-backend.herokuapp.com";
+// const BackendUrl = "http://localhost:4000";
 export default BackendUrl;
